cfec28fb23de8c3845e3b9b56e2ce001f9a9a47b
Fahd Seddik <62207434+FahdSeddik@users.noreply.github.com>
2025-05-15 15:39:09 +0300

    Refactor wmma::fill_fragment calls to use zero instead of static_cast for improved readability

--- pawX/linear_tc.cu
+++ pawX/linear_tc.cu
@@ -65,8 +65,8 @@ __global__ void sklinear_forward_intermediate_wmma(
     wmma::fragment<wmma::accumulator, M, N, K, acc_t> acc1, acc2;
 
     for (int term = 0; term < T; term++) {
-        wmma::fill_fragment(acc1, static_cast<acc_t>(0.0f));
-        wmma::fill_fragment(acc2, static_cast<acc_t>(0.0f));
+        wmma::fill_fragment(acc1, 0);
+        wmma::fill_fragment(acc2, 0);
 #pragma unroll 1
         for (int i = 0; i < TILE_WIDTH_M * TILE_WIDTH_K; i += THREADS_PER_BLOCK) {
             int idx = tid + i;
@@ -142,7 +142,7 @@ __global__ void sklinear_forward_output_wmma(
     wmma::fragment<wmma::matrix_b, M, N, K, half_t, wmma::col_major> fB1, fB2;
     wmma::fragment<wmma::accumulator, M, N, K, acc_t> acc;
     wmma::fragment<wmma::accumulator, M, N, K, acc_t> c_frag;
-    wmma::fill_fragment(acc, static_cast<acc_t>(0.0f));
+    wmma::fill_fragment(acc, 0);
 
     if (hasBias) {
         // load bias into memory N
@@ -288,8 +288,8 @@ __global__ void sklinear_backward_intermediate_wmma(
     wmma::fragment<wmma::matrix_b, M, N, K, half_t, wmma::col_major> fB1, fB2;
     wmma::fragment<wmma::accumulator, M, N, K, acc_t> acc1, acc2;
     for (int term = 0; term < T; term++) {
-        wmma::fill_fragment(acc1, static_cast<acc_t>(0.0f));
-        wmma::fill_fragment(acc2, static_cast<acc_t>(0.0f));
+        wmma::fill_fragment(acc1, 0);
+        wmma::fill_fragment(acc2, 0);
 #pragma unroll 1
         for (int i = 0; i < TILE_WIDTH_M * TILE_WIDTH_K; i += THREADS_PER_BLOCK) {
             int idx = tid + i;
@@ -350,7 +350,7 @@ __global__ void sklinear_backward_grad_S2_interm_wmma(
     wmma::fragment<wmma::matrix_b, M, N, K, half_t, wmma::col_major> fB;
     wmma::fragment<wmma::accumulator, M, N, K, acc_t> acc;
     for (int term = 0; term < T; term++) {
-        wmma::fill_fragment(acc, static_cast<acc_t>(0.0f));
+        wmma::fill_fragment(acc, 0);
 #pragma unroll 1
         for (int i = 0; i < TILE_WIDTH_M * TILE_WIDTH_K; i += THREADS_PER_BLOCK) {
             int idx = tid + i;
@@ -404,7 +404,7 @@ __global__ void sklinear_backward_grad_S2_output_wmma(
     wmma::fragment<wmma::matrix_a, M, N, K, half_t, wmma::col_major> fA;
     wmma::fragment<wmma::matrix_b, M, N, K, half_t, wmma::col_major> fB;
     wmma::fragment<wmma::accumulator, M, N, K, acc_t> acc;
-    wmma::fill_fragment(acc, static_cast<acc_t>(0.0f));
+    wmma::fill_fragment(acc, 0);
     for (int k = 0; k < B; k += TILE_WIDTH_K) {
 #pragma unroll 1
         for (int i = 0; i < TILE_WIDTH_M * TILE_WIDTH_K; i += THREADS_PER_BLOCK) {
@@ -460,8 +460,8 @@ __global__ void sklinear_backward_grad_input_wmma(
     wmma::fragment<wmma::matrix_a, M, N, K, half_t, wmma::col_major> fA1, fA2;
     wmma::fragment<wmma::matrix_b, M, N, K, half_t, wmma::col_major> fB1, fB2;
     wmma::fragment<wmma::accumulator, M, N, K, acc_t> acc1, acc2;
-    wmma::fill_fragment(acc1, static_cast<acc_t>(0.0f));
-    wmma::fill_fragment(acc2, static_cast<acc_t>(0.0f));
+    wmma::fill_fragment(acc1, 0);
+    wmma::fill_fragment(acc2, 0);
 
     for (int term = 0; term < T; term++) {
         for (int k = 0; k < R; k += TILE_WIDTH_K) {
@@ -528,7 +528,7 @@ __global__ void sklinear_backward_grad_S1_wmma(
     wmma::fragment<wmma::matrix_a, M, N, K, half_t, wmma::col_major> fA;
     wmma::fragment<wmma::matrix_b, M, N, K, half_t, wmma::col_major> fB;
     wmma::fragment<wmma::accumulator, M, N, K, acc_t> acc;
-    wmma::fill_fragment(acc, static_cast<acc_t>(0.0f));
+    wmma::fill_fragment(acc, 0);
     for (int k = 0; k < B; k += TILE_WIDTH_K) {
 #pragma unroll 1
         for (int i = 0; i < TILE_WIDTH_M * TILE_WIDTH_K; i += THREADS_PER_BLOCK) {
