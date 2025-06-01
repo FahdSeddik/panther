d0dee85a5170d8921c99bb4287b49fee028ebe13
gaser sami <gaser.elmasry02@eng-st.cu.edu.eg>
2025-06-01 14:20:58 +0300

    debugging

--- panther/tuner/SkAutoTuner/SKAutoTuner.py
+++ panther/tuner/SkAutoTuner/SKAutoTuner.py
@@ -104,6 +104,10 @@ class SKAutoTuner:
             tmp_resolved_configs = self.paramsResolver.resolve(config)
             # this can return list of configs
             resolved_configs.merge(tmp_resolved_configs)
+            print("tmp_resolved_configs")
+            print(tmp_resolved_configs)
+            print("resolved_configs")
+            print(resolved_configs)
 
         return resolved_configs
 
